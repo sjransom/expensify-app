7e017e799a04bdd7e88f3908a03ced218aa19d32
sjransom <sjransom@gmail.com>
2019-07-31 17:21:59 +0100

    redux learning

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import { createStore } from 'redux'
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       }
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      }
+    case 'SET':
+      return {
+        count: action.count
       }
     case 'RESET':
       return {
@@ -19,10 +25,13 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 })
 
-console.log(store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState())
+})
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 })
 
 store.dispatch({
@@ -37,4 +46,12 @@ store.dispatch({
   type: 'DECREMENT'
 })
 
-console.log(store.getState())
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+})
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
