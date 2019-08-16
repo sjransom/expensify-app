c8041949c5cb08646c044f1f36bf84318e52ffd9
james.taylor6@nationwide.co.uk <james.taylor6nationwide.co.uk@jamestaylor6nationwidecouks-MacBook-Pro.local>
2019-08-16 11:53:59 +0100

    adding edit expense and filter expense, also added babel for object spreading

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from 'redux'
-import uuid from 'uuid';
+import uuid from 'uuid'
 
 // ADD_EXPENSE
 const addExpense = (
@@ -26,6 +26,13 @@ const removeExpense = ({ id } = {}) => ({
   id
 })
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+})
+
 // Expenses Reducer
 
 const expensesReducerDefaultState = []
@@ -38,11 +45,29 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
         action.expense
       ]
     case 'REMOVE_EXPENSE':
-      return state.filter(({ id }) => id !== action.id);
+      return state.filter(({ id }) => id !== action.id)
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        }
+        else {
+          return expense
+        }
+      })
     default:
       return state
   }
 }
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
 
 // Filters Reducer
 
@@ -55,6 +80,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
     default:
       return state
   }
@@ -77,6 +107,10 @@ const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter(''))
 
 const demoState = {
   expenses: [{
